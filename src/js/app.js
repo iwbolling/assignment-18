375919df5689f8e9f731dd361a496c3008ed1f8e
iwbolling <iwbolling@gmail.com>
2017-02-22 08:23:18 -0500

    carpools content router completed

--- src/js/app.js
+++ src/js/app.js
@@ -14,28 +14,28 @@ var divFlightTab = document.querySelector(".flight-tab");
 // FUNCTIONS - PAGE CONTENT GENERATION
 function pageContentHome(){
   divRouteContainer.innerHTML = `<div class="home-page">
-                  <div class="home-page_content">
-                    <h2>The Basic Facts</h2>
-                    <table class="facts-table">
-                      <tr>
-                        <td>Native Name</td>
-                        <td>Ísland</td>
-                      </tr>
-                      <tr>
-                        <td>Demonym</td>
-                        <td>Icelander</td>
-                      </tr>
-                      <tr>
-                        <td>Area</td>
-                        <td>102,775 km²</td>
-                      </tr>
-                      <tr>
-                        <td>Country Code</td>
-                        <td>+354</td>
-                      </tr>
-                    </table>
-                  </div>
-                </div>`
+                                  <div class="home-page_content">
+                                    <h2>The Basic Facts</h2>
+                                    <table class="facts-table">
+                                      <tr>
+                                        <td>Native Name</td>
+                                        <td>Ísland</td>
+                                      </tr>
+                                      <tr>
+                                        <td>Demonym</td>
+                                        <td>Icelander</td>
+                                      </tr>
+                                      <tr>
+                                        <td>Area</td>
+                                        <td>102,775 km²</td>
+                                      </tr>
+                                      <tr>
+                                        <td>Country Code</td>
+                                        <td>+354</td>
+                                      </tr>
+                                    </table>
+                                  </div>
+                                </div>`
 };
 function pageContentConcerts(){
   $.getJSON("http://apis.is/concerts").then(function(serverRes){
@@ -61,23 +61,31 @@ function pageContentConcerts(){
                                     </div>
                                   </div>`
   });
-}
-
-var pageContentObj = {
-  concerts: `<div class="concerts-page">
-                <div class="concerts-page_content">
-                  <h2>Concerts</h2>
-                  <div class="all-concerts row">
-                    <div class="concert-pane col-lg-4">
-                      <div class="concert-pane_thumb thumbnail">
-                        <h3>PLACEHOLDER</h3>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-             </div>`,
-  carpools: ``,
-  flights: ``
+};
+function pageContentCarpools(){
+  $.getJSON("http://apis.is/rides/samferda-drivers/").then(function(serverRes){
+    let carpoolRes = serverRes.results;
+    var genCarpools = carpoolRes.map(function(carpoolObj){
+      return `<tr>
+                <td>${carpoolObj.time}, ${carpoolObj.date}</td>
+                <td>${carpoolObj.from}</td>
+                <td>${carpoolObj.to}</td>
+              </tr>`
+    }).join("");
+    divRouteContainer.innerHTML = `<div class="carpools-page">
+                                    <div class="carpools-page_content">
+                                      <h2>Carpools</h2>
+                                      <table class="carpools-table">
+                                        <tr>
+                                          <th>Time of Departure</th>
+                                          <th>From</th>
+                                          <th>To</th>
+                                        </tr>
+                                        ${genCarpools}
+                                      </table>
+                                    </div>
+                                  </div>`
+  });
 };
 
 // FUNCTIONS - PAGE CONTENT ROUTER
@@ -86,6 +94,8 @@ function pageContentRouter(){
     pageContentHome();
   } else if (window.location.hash === "#concerts"){
     pageContentConcerts();
+  } else if (window.location.hash === "#carpools"){
+    pageContentCarpools();
   };
 }
 pageContentRouter();
